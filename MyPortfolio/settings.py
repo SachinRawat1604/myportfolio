121c210c97730f6d734d046c66bf3e44ec209e42
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-03-25 15:17:33 +0530

    code Change

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'MyPortfolio.urls'
+ROOT_URLCONF = 'myportfolio.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MyPortfolio.wsgi.application'
+WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 
 # Database
