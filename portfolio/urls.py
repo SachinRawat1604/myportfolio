0a1db5b4ae3b3a736e8e7d11f921614c72e8297d
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-03-25 23:01:36 +0530

    fix urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('education/', views.education, name='education'),
-    path('skills/', views.skills, name='skills'),
-
+    path('', views.home, name='home'),
+    path('projects/', views.projects, name='projects'),
+    path('experience/', views.experience, name='experience'),
+    path('certifications/', views.certifications, name='certifications'),
+    path('contact/', views.contact, name='contact'),
 ]
