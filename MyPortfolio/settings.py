4efea9c0614837ff311e28b308b76cdcbc4b9192
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-03-26 11:46:24 +0530

    fix: static files config with whitenoise

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'MyPortfolio.urls'
@@ -126,6 +127,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
