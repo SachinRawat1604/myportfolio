a617675e6a166523376b139f3077409527c96afa
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-03-29 14:54:29 +0530

    fix

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.shortcuts import render
 from django.conf import settings
+from decouple import config
 
 def home(request):
     return render(request, 'base.html') 
@@ -17,17 +18,20 @@ def experience(request):
 def certifications(request):
     return render(request, 'certifications.html')
 
-def contact_view(request):
+def contact(request):
     success = False
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         
-        subject = f"New Contact from {name}"
-        full_message = f"Message from {name} ({email}):\n\n{message}"
-
-        send_mail(subject, full_message, settings.DEFAULT_FROM_EMAIL, [settings.CONTACT_EMAIL])
+        send_mail(
+            subject=f'Contact Form Submission from {name}',
+            message=message,
+            from_email=email,
+            recipient_list=[config('EMAIL_HOST_USER')],
+        )
+        
         success = True
 
     return render(request, 'contact.html', {'success': success})
